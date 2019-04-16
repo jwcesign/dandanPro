156a12dc2fb909f309387567037c0f84a78c15f6
jiang wei <jwcesign@gmail.com>
2019-04-16 22:37:16 +0800

    Add nginx file
    Author: jiangwei
    Data: 2018-04-16

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dandanPro
-asdasd
 反身网络科技库存监控项目---正式工作前
+
+### 工作进度
+* nginx安装脚本(centos)---doing
---
+++ en_install/nginx_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# change dir
+cd ./software_tar
+tar -zxvf nginx-1.12.2.tar.gz
+cd nginx-1.12.2
